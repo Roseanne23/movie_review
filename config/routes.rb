3d7bd159904170bfc6d56f01f7c1fde2f93a489e
Roseanne23 <roseanne.gulosino@gmail.com>
2024-10-15 22:07:09 +0800

    Add: User can add a movie

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
+  resources :movies, only: [:new, :create, :show]
   root 'welcome#index'
 end
